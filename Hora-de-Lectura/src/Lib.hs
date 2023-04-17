49c4bab49bad1a9f8e43b32926db3787e551ac64
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-04-17 17:10:25 -0300

    Refactorizacion del codigo, aplicando el 'data' para definir el tipo de dato 'Libro'

--- Hora-de-Lectura/src/Lib.hs
+++ Hora-de-Lectura/src/Lib.hs
@@ -9,62 +9,58 @@ module Lib
 type Nombre = String
 type Autor = String
 type Paginas = Int
-type Libro = (Nombre, Autor, Paginas)
 type Biblioteca = [Libro]
 type Saga = [Libro]
 
+-- Declaracion del tipo de dato Libro
+-- Funciones Accessors (estan implicitos en la declaracon del tipo de dato Libro -record syntax-)
+data Libro = Libro {
+    nombre :: Nombre,
+    autor :: Autor,
+    paginas :: Paginas 
+} deriving (Eq, Show)
+
 -- Declaracion de los libros
 elVisitante :: Libro
-elVisitante = ("El visitante", "Stephen King", 592)
+elVisitante = Libro "El visitante" "Stephen King" 592
 
 snk1 :: Libro
-snk1 = ("SNK-1", "Hajime Isayama", 40)
+snk1 = Libro "SNK-1" "Hajime Isayama" 40
 
 snk3 :: Libro
-snk3 = ("SNK-3", "Hajime Isayama", 40)
+snk3 = Libro "SNK-3" "Hajime Isayama" 40
 
 snk127 :: Libro
-snk127 = ("SNK-127", "Hajime Isayama", 40)
+snk127 = Libro "SNK-127" "Hajime Isayama" 40
 
 fundacion :: Libro
-fundacion = ("Fundacion", "Isaac Asimov", 230)
+fundacion = Libro "Fundacion" "Isaac Asimov" 230
 
 sandman5 :: Libro
-sandman5 = ("Sandman-5", "Neil Gaiman", 35)
+sandman5 =  Libro"Sandman-5" "Neil Gaiman" 35
 
 sandman10 :: Libro
-sandman10 = ("Sandman-10", "Neil Gaiman", 35)
+sandman10 = Libro "Sandman-10" "Neil Gaiman" 35
 
 sandman12 :: Libro
-sandman12 = ("Sandman-12", "Neil Gaiman", 35)
+sandman12 = Libro "Sandman-12" "Neil Gaiman" 35
 
 eragon :: Libro
-eragon = ("Eragon", "Christopher Paolini", 544)
+eragon = Libro "Eragon" "Christopher Paolini" 544
 
 eldest :: Libro
-eldest = ("Eldest", "Christopher Paolini", 704)
+eldest = Libro "Eldest" "Christopher Paolini" 704
 
 brisignr :: Libro
-brisignr = ("Brisignr","Christopher Paolini", 700)
+brisignr = Libro "Brisignr""Christopher Paolini" 700
 
 legado :: Libro
-legado = ("Legado","Christopher Paolini", 811)
+legado = Libro "Legado""Christopher Paolini" 811
 
 
 -- Mi biblioteca
 biblioteca :: Biblioteca
 biblioteca = [elVisitante, snk1, snk3, snk127, fundacion, sandman5, sandman10, sandman12, eragon, eldest, brisignr, legado]
-
--- Funciones Accessors
-nombre :: Libro -> Nombre
-nombre (nombre, _, _) = nombre
-
-autor :: Libro -> Autor
-autor (_, autor, _) = autor
-
-paginas :: Libro -> Int
-paginas (_ ,_ , paginas) = paginas 
-
 
 -- Promedio de las hojas de mi biblioteca
 promedioDeHojas :: Biblioteca -> Paginas
@@ -78,7 +74,9 @@ listaDePaginas biblioteca  = map paginas biblioteca
 
 -- Lectura Obligatoria
 lecturaObligatoria :: Libro -> Bool
-lecturaObligatoria libro = autor libro == "Stephen King" || perteneceSagaEragon libro || esMismoLibro libro fundacion
+lecturaObligatoria (Libro _ "Stephen King" _) = True
+lecturaObligatoria (Libro "Fundacion" "Isaac Asimov" 230) = True
+lecturaObligatoria unLibro = perteneceSagaEragon unLibro
 
 sagaEragon :: Saga
 sagaEragon = [eragon, eldest, brisignr, legado]
@@ -86,20 +84,15 @@ sagaEragon = [eragon, eldest, brisignr, legado]
 perteneceSagaEragon :: Libro -> Bool
 perteneceSagaEragon libro = elem libro sagaEragon
 
-esMismoLibro :: Libro -> Libro -> Bool
-esMismoLibro unLibro otroLibro = nombre unLibro == nombre otroLibro &&
-                                autor unLibro == autor otroLibro &&
-                                paginas unLibro == paginas otroLibro
 
 -- Biblioteca fantasiosa
 esBibliotecaFantasiosa :: Biblioteca -> Bool
 esBibliotecaFantasiosa biblioteca = any esLibroFantasioso biblioteca
 
-esDeAutor :: Libro -> Autor -> Bool -- Funcion auxiliar para saber si un libro es de un autor
-esDeAutor unLibro unAutor = autor unLibro == unAutor
-
 esLibroFantasioso :: Libro -> Bool
-esLibroFantasioso unLibro = esDeAutor unLibro "Christopher Paolini" || esDeAutor unLibro "Neil Gaiman"
+esLibroFantasioso (Libro _ "Christopher Paolini" _) = True 
+esLibroFantasioso (Libro _ "Neil Gaiman" _ ) = True
+esLibroFantasioso (Libro {}) = False
 
 
 -- Nombre de la biblioteca
@@ -110,29 +103,25 @@ sacarVocales :: Nombre -> Nombre
 sacarVocales nombre = filter (`notElem` "aAeEiIoOuU -áÁéÉíÍóÓúÚ") nombre
 
 unirNombres :: Biblioteca -> Nombre
-unirNombres biblioteca =  (concat.listaDeNombres) biblioteca
--- uniNombres biblioteca = concatMap nombre biblioteca
-
-listaDeNombres :: Biblioteca -> [Nombre]
-listaDeNombres biblioteca = map nombre biblioteca
+unirNombres biblioteca =  concatMap nombre biblioteca
 
 -- Biblioteca Ligera
 bibliotecaLigera :: Biblioteca -> Bool
-bibliotecaLigera biblioteca = all esLibroLigero biblioteca
+bibliotecaLigera biblioteca = all lecturaLigera biblioteca
 
-esLibroLigero :: Libro -> Bool
-esLibroLigero libro = paginas libro < 40
+lecturaLigera :: Libro -> Bool
+lecturaLigera libro = paginas libro < 40
 
 -- Funcion que devuelve el genero del libro
 genero :: Libro -> String
 genero unLibro 
     | autor unLibro == "Stephen King" = "Terror"
     | esAutorJapones unLibro = "Manga"
-    | esLibroLigero unLibro = "Comic"
+    | lecturaLigera unLibro = "Comic"
     | otherwise = "No clasificado"
 
 autoresJaponeses :: [Autor]
 autoresJaponeses = ["Hajime Isayama"]
 
 esAutorJapones :: Libro -> Bool
-esAutorJapones (_, autor, _) = autor `elem` autoresJaponeses 
+esAutorJapones (Libro _ autor _) = autor `elem` autoresJaponeses 
