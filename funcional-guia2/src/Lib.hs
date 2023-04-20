5eeb7867572be916e1747713020773f444c6686d
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-04-19 22:31:07 -0300

    Resolucion de la guia 2 funcional, hasta el ejercicio 9

--- funcional-guia2/src/Lib.hs
+++ funcional-guia2/src/Lib.hs
@@ -1,6 +1,47 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-Wunused-top-binds #-}
+{-# OPTIONS_GHC -Wno-Wtype-defaults #-}
+{-# HLINT ignore "Use infix" #-}
+{-# HLINT ignore "Eta reduce" #-}
+
 module Lib
-    ( someFunc
+    ( 
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- APLICACION PARCIAL
+-- 1) Funcion siguiente
+siguiente :: Num a => a -> a 
+siguiente = (+1)
+
+-- 2) Funcion mitad
+mitad :: Float -> Float
+mitad = (/2)
+
+-- 3) Funcion inversa
+inversa :: Float -> Float
+inversa = (1/)
+
+-- 4) Funcion triple
+triple :: Num a => a -> a
+triple = (*3)
+
+--5) Funcion esNumeroPositivo
+esNumeroPositivo :: (Ord a, Num a) => a -> Bool
+esNumeroPositivo = (>0)
+
+-- COMPOSICION
+-- 6) Es multiplo de 2 
+esMultiploDe2 :: Int -> Bool
+esMultiploDe2 = (==0) . (`mod` 2)
+
+-- 7) Es un numero bisiesto 
+esBisiesto :: Int -> Bool
+esBisiesto  año = ((==0) . (`mod` 400)) año || ((==0).(`mod` 4)) año && ((/=0).(`mod` 100)) año -- Poco declarativo y expresivo(?)
+
+--8) Inversa de la raiz cuadrada
+inversaRaizCuadrada :: Float -> Float
+inversaRaizCuadrada = inversa . sqrt
+
+-- 9) Incremento M al cuadrado de N
+incrementMCuadradoN :: Num a => a -> a -> a 
+incrementMCuadradoN numeroM numeroN = (+numeroN) . (^2) $ numeroM
