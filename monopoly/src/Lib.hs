c9c743f4e80e80ae958887d5189592635a0f71b5
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-04-24 13:39:50 -0300

    Declaracion de los tipos de datos y los alias: Jugador, Propiedad y Accion

--- monopoly/src/Lib.hs
+++ monopoly/src/Lib.hs
@@ -1,6 +1,36 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
 module Lib
-    ( someFunc
+    ( 
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Show.Functions ()
+
+type Nombre = String
+type Dinero = Int
+type Tactica = String
+
+data Jugador = Jugador {
+    nombre      :: Nombre,
+    dinero      :: Dinero,
+    tactica     :: Tactica,
+    propiedades :: [Propiedad],
+    acciones    :: [Accion]
+}  deriving Show
+
+data Propiedad = Propiedad {
+    nombrePropiedad :: Nombre,
+    precio          :: Dinero
+} deriving Show
+
+--data Accion = UnaAccion{ 
+--    pagarAAccionista    :: Jugador -> Jugador,
+--    enojarse            :: Jugador -> Jugador,
+--    pasarPorElBanco     :: Jugador -> Jugador,
+--    gritar              :: Jugador -> Jugador,
+--    subastar            :: Jugador -> Jugador,
+--    cobrarAlquileres    :: Jugador -> Jugador
+--} ¿¿se puede hacer hacer Accion con un data constructors??
+
+type Accion = Jugador -> Jugador 
+
