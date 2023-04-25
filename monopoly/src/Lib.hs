a21467bd3785222b83bdf5b2f516ffc52760facc
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-04-25 16:53:56 -0300

    Formateo automático del codigo (esta bueno?)

--- monopoly/src/Lib.hs
+++ monopoly/src/Lib.hs
@@ -1,61 +1,69 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Eta reduce" #-}
 module Lib
-    ( 
-    ) where
+  (
+  )
+where
 
 import Text.Show.Functions ()
 
 type Nombre = String
+
 type Dinero = Int
+
 type Tactica = String
 
-data Jugador = Jugador {
-    nombre      :: Nombre,
-    dinero      :: Dinero,
-    tactica     :: Tactica,
+data Jugador = Jugador
+  { nombre :: Nombre,
+    dinero :: Dinero,
+    tactica :: Tactica,
     propiedades :: [Propiedad],
-    acciones    :: [Accion]
-}  deriving Show
+    acciones :: [Accion]
+  }
+  deriving (Show)
 
-data Propiedad = Propiedad {
-    nombrePropiedad :: Nombre,
-    precio          :: Dinero
-} deriving Show
+data Propiedad = Propiedad
+  { nombrePropiedad :: Nombre,
+    precio :: Dinero
+  }
+  deriving (Show)
 
---data Accion = UnaAccion{ 
+-- data Accion = UnaAccion{
 --    pagarAAccionista    :: Jugador -> Jugador,
 --    enojarse            :: Jugador -> Jugador,
 --    pasarPorElBanco     :: Jugador -> Jugador,
 --    gritar              :: Jugador -> Jugador,
 --    subastar            :: Jugador -> Jugador,
 --    cobrarAlquileres    :: Jugador -> Jugador
---} ¿¿se puede hacer hacer Accion con un data constructors?? no tiene sentido porque no es un tipo de dato
+-- } ¿¿se puede hacer hacer Accion con un data constructors?? no tiene sentido porque no es un tipo de dato
 
-type Accion = Jugador -> Jugador 
+type Accion = Jugador -> Jugador
 
 -- Defino a los jugadores
 carolina :: Jugador
-carolina = Jugador {
-    nombre = "Carolina",
-    dinero = 500,
-    tactica = "Accionista",
-    propiedades = [],
-    acciones = [ pagarAccionistas ]
-}
+carolina =
+  Jugador
+    { nombre = "Carolina",
+      dinero = 500,
+      tactica = "Accionista",
+      propiedades = [],
+      acciones = [pagarAccionistas]
+    }
 
 manuel :: Jugador
-manuel = Jugador{
-    nombre = "Manuel",
-    dinero = 500,
-    tactica = "Oferente singular",
-    propiedades = [],
-    acciones = [enojarse]
-}
+manuel =
+  Jugador
+    { nombre = "Manuel",
+      dinero = 500,
+      tactica = "Oferente singular",
+      propiedades = [],
+      acciones = [enojarse]
+    }
 
 -- Declaracion de las ACCIONES
 pasarPorElBanco :: Accion
-pasarPorElBanco  unJugador =  cambiarTacticaACompradorCompulsivo . sumarDinero 40 $ unJugador
+pasarPorElBanco unJugador = cambiarTacticaACompradorCompulsivo . sumarDinero 40 $ unJugador
 
 sumarDinero :: Int -> Jugador -> Jugador
 sumarDinero cantidad unJugador = unJugador {dinero = dinero unJugador + cantidad}
@@ -63,10 +71,10 @@ sumarDinero cantidad unJugador = unJugador {dinero = dinero unJugador + cantidad
 cambiarTacticaACompradorCompulsivo :: Jugador -> Jugador
 cambiarTacticaACompradorCompulsivo unJugador = unJugador {tactica = "Comprador compulsivo"}
 
-pagarAccionistas :: Accion 
-pagarAccionistas unJugador 
-    | tactica unJugador == "Accionista" = sumarDinero 200 unJugador
-    | otherwise = restarDinero 100 unJugador
+pagarAccionistas :: Accion
+pagarAccionistas unJugador
+  | tactica unJugador == "Accionista" = sumarDinero 200 unJugador
+  | otherwise = restarDinero 100 unJugador
 
 restarDinero :: Int -> Jugador -> Jugador
 restarDinero cantidad unJugador = unJugador {dinero = dinero unJugador - cantidad}
@@ -80,11 +88,17 @@ agregarAccion unaAccion unJugador = unJugador {acciones = unaAccion : acciones u
 gritar :: Accion
 gritar unJugador = unJugador {nombre = "AHHHH" ++ nombre unJugador}
 
-subastar :: Propiedad -> Accion --hacer
-subastar  unaPropiedad unJugador 
-    | tactica unJugador == "Oferente singular" || tactica unJugador == "Accionista" = pagarPropiedad unaPropiedad unJugador
-    | otherwise = unJugador
+subastar :: Propiedad -> Accion
+subastar unaPropiedad unJugador
+  | tactica unJugador == "Oferente singular" || tactica unJugador == "Accionista" = pagarPropiedad unaPropiedad unJugador
+  | otherwise = unJugador
 
 pagarPropiedad :: Propiedad -> Jugador -> Jugador
-pagarPropiedad unaPropiedad unJugador = unJugador { propiedades = unaPropiedad : propiedades unJugador, 
-                                                    dinero = dinero unJugador - precio unaPropiedad }
+pagarPropiedad unaPropiedad unJugador =
+  unJugador
+    { propiedades = unaPropiedad : propiedades unJugador,
+      dinero = dinero unJugador - precio unaPropiedad
+    }
+
+cobrarAlquileres :: Accion
+cobrarAlquileres unJugador = sumar . valoresDePropiedades $ unJugador
