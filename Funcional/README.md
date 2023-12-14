831aba3cfa310370dec3f1e6cd04a1ad22bca85b
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-12-14 13:43:59 -0300

    .

--- Funcional/README.md
+++
@@ -1,6 +0,0 @@
-# Paradigmas de Programación
-
-### Eduardo Onishi ( eonishi@frba.utn.edu.ar )
-
-### Funcional :
-<image src="https://i.redd.it/eyvhhjkdb3671.jpg" alt="Haskell es funcional">
--- Funcional/padrinos-magicos/src/Lib.hs
+++ Funcional/padrinos-magicos/src/Lib.hs
@@ -1,6 +1,56 @@
-module Lib
-    ( someFunc
-    ) where
+import Text.Show.Functions ()
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Chico = Chico
+    {
+        nombre :: String,
+        edad:: Int,
+        habilidades:: Habilidades,
+        deseos:: [Deseo]
+    } deriving Show
+
+type Deseo = Chico -> Chico
+type Habilidades = [String]
+
+carlos :: Chico
+carlos = Chico
+    {
+        nombre = "Carlos",
+        edad = 12,
+        habilidades = ["Comer", "tomar", "Domir"],
+        deseos = [serMayor, aprenderHabilidades ["Correr"], aprenderHabilidades ["Algo"]]
+    }
+
+
+aprenderHabilidades :: Habilidades -> Deseo
+aprenderHabilidades unashabilidades unChico = unChico{habilidades = unashabilidades ++ habilidades unChico }
+
+serGrosoEnNeedForSpeed :: Deseo
+serGrosoEnNeedForSpeed = aprenderHabilidades todosLosNeedForSpeed 
+
+todosLosNeedForSpeed :: [String]
+todosLosNeedForSpeed = map(\unNumero -> "jugar NSF" ++ show unNumero) [1..]
+
+--serMayor :: Deseo
+--serMayor = cambiarEdad (=18)
+
+cambiarEdad :: Num a =>   Chico -> ((a -> a) -> a) -> Chico
+cambiarEdad unChico alteracionDeEdad  = unChico {edad = alteracionDeEdad (edad unChico) }
+
+type PadrinoMagico = Chico -> Chico
+
+edadActual :: Chico -> Int
+edadActual = edad
+
+primerDeseo :: Chico -> Deseo
+primerDeseo = head.deseos
+
+cumplirDeseo :: Deseo -> Chico -> Chico
+cumplirDeseo unDeseo = unDeseo
+
+cumplirPrimerDeseo :: Chico -> Chico
+cumplirPrimerDeseo pibe = cumplirDeseo (primerDeseo pibe) pibe
+
+--wanda :: PadrinoMagico
+--wanda pibe = cambiarEdad(edadActual pibe +1).cumplirPrimerDeseo $ pibe
+
+
