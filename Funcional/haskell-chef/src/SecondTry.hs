8cd8ae9beb1873992612d8492dca0f7de7c6c892
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-12-14 13:18:49 +0000

    Agrego mi segundo intento resolviendo HaskellChef

---
+++ Funcional/haskell-chef/src/SecondTry.hs
@@ -0,0 +1,40 @@
+data Participante = Participante {
+    nombre :: String,
+    trucos :: [Truco],
+    especialidad :: Plato
+}
+
+data Plato = Plato {
+    dificultad :: Int,
+    componentes :: [Componente]
+}
+
+type Truco = Plato -> Plato 
+type Componente = (Ingrediente, Int)
+type Ingrediente = String
+
+---------------------------
+
+modificarComponentes :: ([Componente] -> [Componente]) -> Plato -> Plato
+modificarComponentes unaFuncion plato = plato {componentes = unaFuncion.componentes $ plato} 
+
+agregarComponente :: Ingrediente -> Int -> Plato
+agregarComponente ingrediente gramos = modificarComponentes (\componentes => (ingrediente,gramos):componentes) 
+
+endulzar :: Int -> Truco
+endulzar gramos = agregarComponente "Azucar" gramos
+
+salar :: Int -> Truco
+salar gramos = agregarComponente "Sal" gramos
+
+darSabor :: Int -> Int -> Truco
+darSabor gramosAzucar gramosSal = endulzar gramosAzucar . salar gramosSal
+
+duplicarPorcion :: Truco
+duplicarPorcion unPlato = modificarComponentes (map duplicarCantidad)
+
+duplicarCantidad :: Componente -> Componente
+duplicarCantidad (ingrediente, cantidad ) = (ingrediente, cantidad*2) 
+
+simplificar :: Truco
+simplificar 
